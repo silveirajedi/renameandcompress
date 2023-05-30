5964eb3c58ee79d346ae276f7c0ecaa528bc5182
Leandro Silveira <silveira.jedi@gmail.com>
2023-05-30 10:03:22 -0300

    Comentários no config

--- config.php
+++ config.php
@@ -1,8 +1,12 @@
 <?php
-
+//diretórios que devem ser criados na pasta do projeto
 const DIRETORIO = "images";
 const CACHE = "images/cache";
 const JPGFILES = "images/jpgfiles";
+
+//colocar o nome que serão renomeados os arquivos
 const NOVONOME = "Teste";
+
+//escolher resolução ideal
 const WIDTH = "300";
 const HEIGHT = null;
